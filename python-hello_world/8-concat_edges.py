4af7c05c137b455cb8a05ca6fb5148ecb02404a1
dami123-del <d.oluwadami@alustudent.com>
2026-04-23 19:33:00 -0700

    learning_python_basics-8

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[54:66] + str[200:190] + str[0:4]
+str = str[39:66] + str[106:112] + str[0:6]
 print(str)
