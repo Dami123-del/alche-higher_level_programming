02b5ec1b160418251fe56351e3a876e83487891d
dami123-del <d.oluwadami@alustudent.com>
2026-04-26 11:44:00 -0700

    python-data_structures-11

---
+++ python-data_structures/11-delete_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    # Check if the index is within the valid range
+    if idx >= 0 and idx < len(my_list):
+        # Delete the item at the specified position
+        del my_list[idx]
+
+    # Return the list (modified or original)
+    return my_listi
