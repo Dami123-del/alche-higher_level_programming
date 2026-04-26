83885981909e13d8760df2b2c8df76b21589309a
dami123-del <d.oluwadami@alustudent.com>
2026-04-26 00:03:54 -0700

    if_else_loop-6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i + 1, 10):
-        print("{:d}{:d}".format(i, j), end=", " if i != 8 or j != 9 else "\n")
-
+        if i == 8 and j == 9:
+            # The final combination followed by a newline
+            print("{}{}".format(i, j))
+        else:
+            # All other combinations followed by a comma and space
+            print("{}{}".format(i, j), end=", ")
