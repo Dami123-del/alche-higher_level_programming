52a1ad3735e4c0888582a206a6c940118c9dd468
dami123-del <d.oluwadami@alustudent.com>
2026-04-26 10:20:40 -0700

    python-data_structures-4

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,13 @@
+#!usr/bin/python3
+def new_in_list(my_list, idx, element):
+    # Create a copy of the original list
+    copy_list = my_list[:]
+
+    # Check if idx is negative or out of range
+    if idx < 0 or idx >= len(my_list):
+        return copy_list
+
+    # Replace the element in the copy
+    copy_list[idx] = element
+
+    return copy_list
