b71fc7530414e4fafe2176595bf2c4b77afa0804
dami123-del <d.oluwadami@alustudent.com>
2026-05-03 13:59:19 -0700

    python-more_data_structures-0

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for row in matrix:
+        new_row = []
+        for num in row:
+            new_row.append(num ** 2)
+        new_matrix.append(new_row)
+    ireturn new_matrix
