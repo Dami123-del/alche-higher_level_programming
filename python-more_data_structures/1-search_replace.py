75d0b831a43c66351fc08a17ebca789616d3db87
dami123-del <d.oluwadami@alustudent.com>
2026-05-04 01:40:43 -0700

    python-more_data_structures-1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 def search_replace(my_list, search, replace):
     # Create a new list using a conditional list comprehension
     return [replace if item == search else item for item in my_list]
