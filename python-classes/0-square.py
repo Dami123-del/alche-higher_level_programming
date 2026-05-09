7f49aec0da159363708f9c2c3895fd6b38747ca9
dami123-del <d.oluwadami@alustudent.com>
2026-05-09 10:24:48 -0700

    python_classes-1

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """
-This module provides a simple Square class.
+Module that defines a Square class.
 """
+
 
 class Square:
     """
-    An empty class that defines a square.
+    Class that defines a square.
     """
     pass
