9095eea442d4c22a4d1bb267ea632264d5625c38
dami123-del <d.oluwadami@alustudent.com>
2026-06-02 12:15:22 -0700

    working on inheritance-5

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module defines the BaseGeometry class.
+
+It serves as the base layer foundation for future geometry-related
+class extensions and operations.
+"""
+
+
+class BaseGeometry:
+    """An empty base class for geometric operations."""
+
+    pass
